19577d45da1217dd5072aac06fa6436c14a96d06
Elaine Danahy <edanahy22@gmail.com>
2022-07-14 21:27:14 -0500

    added yelp to main handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['first_name'],
         },
       ],
     });
@@ -33,7 +33,7 @@ router.get('/trip/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ['first_name'],
         },
       ],
     });
