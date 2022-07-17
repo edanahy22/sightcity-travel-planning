d645a248c061d9f21e8a469c68c0b3167c680854
ian.moss <ianmsieg@gmail.com>
2022-07-16 22:45:08 -0500

    changed main template button to have a plan new trip button, changed profile template to display saved trips on lefthand side, changed newtrip template to dynamically list trips made by all users except current user

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Trip, User } = require('../models');
+const {Sequelize, Op} = require('sequelize');
+const { Trip, User, Hotel, Activity} = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -48,32 +49,32 @@ router.get('/', async (req, res) => {
 //   }
 // });
 
-router.get('/trip', withAuth, async (req, res) => {
-  try {
-    const tripData = await Trip.findAll({
-      where: {
-        user_id: req.session.user_id
-      },
-  });
+// router.get('/trip', withAuth, async (req, res) => {
+//   try {
+//     const tripData = await Trip.findAll({
+//       where: {
+//         user_id: req.session.user_id
+//       },
+//   });
 
-    const trips = tripData.map((trip) => trip.get({ plain: true}));
+//     const trips = tripData.map((trip) => trip.get({ plain: true}));
 
-    res.render('alltrips', {
-      trips,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('alltrips', {
+//       trips,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/trip/:id', withAuth, async (req, res) => {
   try {
     const tripData = await Trip.findByPk(req.params.id, {
       include: [
         {
-          model: User,
-          attributes: ['first_name']
+          model: Hotel,
+          //model: Activity
         }
       ]
     });
@@ -91,7 +92,22 @@ router.get('/trip/:id', withAuth, async (req, res) => {
 
 router.get('/newtrip', withAuth, async (req, res) => {
   try {
+    const tripData = await Trip.findAll({
+      where: {
+        user_id: {
+          [Op.ne]: `${req.session.user_id}`
+        }
+      },
+      //Working on including the user's first name who created the trip=============================================
+      include : [{
+        model: User,
+        attributes: ['first_name']
+      }],
+    })
+    const trips = tripData.map((trip) => trip.get({ plain: true }));
+    console.log(trips)
     res.status(200).render('newtrip', {
+      trips,
       logged_in: req.session.logged_in
     })
   } catch (err) {
