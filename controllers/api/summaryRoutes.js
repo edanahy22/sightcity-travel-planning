d97ba576c6172c2784b6ce7305efe2a0fa07819d
ian.moss <ianmsieg@gmail.com>
2022-07-20 16:31:02 -0500

    submit email now has very ugly table render in console on click

--- controllers/api/summaryRoutes.js
+++ controllers/api/summaryRoutes.js
@@ -12,11 +12,19 @@ router.get('/', withAuth, async (req, res) => {
       ],
       // order: [['start_date','ASC']],
     });
-    console.log(tripData);
 
     const userData = await User.findByPk(req.session.user_id);
     const user = userData.get({ plain: true });
+    
     const trip = tripData.get({ plain: true });
+    let start_date = new Date(trip.start_date)
+    trip.start_date = start_date
+    let end_date = new Date(trip.end_date)
+    trip.end_date = end_date
+    for (let i = 0; i < trip.activities.length; i++) {
+      let activity_date = new Date(trip.activities[i].activity_date).toDateString()
+      trip.activities[i].activity_date = activity_date
+    }
 
     res.json({user, trip});
   } catch (err) {
--- public/js/email.js
+++ public/js/email.js
@@ -3,22 +3,22 @@ function emailInit() {
 }
 emailInit();
 
-window.onload = function() {
-    document.getElementById('itinerary-form').addEventListener('submit', function(event) {
-        event.preventDefault();
+// window.onload = function() {
+//     document.getElementById('itinerary-form').addEventListener('submit', function(event) {
+//         event.preventDefault();
 
-        this.contact_number.value = Math.random() * 100000 | 0;
-        this.from_name.value = 'SightCity Travel';
-        this.message.value = `Are you ready for your trip to <h3 style ="color: purple">${this.destination.value}</h3>\nfrom <b>${this.start_date.value}</b> to <b>${this.end_date.value}</b>\nYou will be staying at\n<h6 style="color: aqua">${this.hotel.value}</h6>`;
+//         this.contact_number.value = Math.random() * 100000 | 0;
+//         this.from_name.value = 'SightCity Travel';
+//         this.message.value = `Are you ready for your trip to <h3 style ="color: purple">${this.destination.value}</h3>\nfrom <b>${this.start_date.value}</b> to <b>${this.end_date.value}</b>\nYou will be staying at\n<h6 style="color: aqua">${this.hotel.value}</h6>`;
 
-        emailjs.sendForm('service_e25c33t', 'trip_itinerary', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
+//         emailjs.sendForm('service_e25c33t', 'trip_itinerary', this)
+//             .then(function() {
+//                 console.log('SUCCESS!');
+//             }, function(error) {
+//                 console.log('FAILED...', error);
+//             });
+//     });
+// }
 
 //date function
 Date.prototype.addDays = function(days) {
@@ -31,16 +31,16 @@ function getDates(startDate, stopDate) {
     var dateArray = new Array();
     var currentDate = startDate;
     while (currentDate <= stopDate) {
-        dateArray.push(new Date (currentDate));
+        dateArray.push(new Date (currentDate).toDateString());
         currentDate = currentDate.addDays(1);
     }
     return dateArray;
 }
 
 //Input trip dates and activities
-function emailFormat(trip, activities) {
-    const start_date = trip.start_date;
-    const end_date = trip.end_date;
+function emailFormat(data) {
+    const start_date = new Date(data.trip.start_date);
+    const end_date = new Date(data.trip.end_date);
     const tripDates = getDates(start_date, end_date)
     let tableTextArr = []
     // let tableRowsArr = []
@@ -50,13 +50,32 @@ function emailFormat(trip, activities) {
     }
     //Creates table data tag with the image, name and address of the activity if the activity date matches the trip date in the calendar
     for (let i=0; i<tripDates.length; i++) {
-        for (let j=0; j<activities.length; j++) {
-            if(activities[j].activity_date == tripDates[i]) {
-                tableTextArr[i] + `<tr><img src="${activities[i].activity_img}">\n <td>${activities[i].activity_name}\n${activities[i].activity_address}\n${activities[i]}</td></tr>`
+        for (let j=0; j<data.trip.activities.length; j++) {
+            if(data.trip.activities[j].activity_date == tripDates[i]) {
+                tableTextArr[i] = tableTextArr[i] + `<tr><td><img src="${data.trip.activities[j].activity_img}"></td>\n <td>${data.trip.activities[j].activity_name}</td><td>${data.trip.activities[j].activity_address}</td><td>${data.trip.activities[j]}</td></tr>`
+            } else {
+                tableTextArr[i] = tableTextArr[i] + `<tr><td>No activities planned</td></tr>`
             }
         }
     }
+    return tableTextArr.join(' ')
 }
+
+let emailBtn = document.getElementById('email-button')
+emailBtn.addEventListener('click', callData)
+
+function callData(e) {
+    e.preventDefault();
+    fetch('/api/summary')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        let test = emailFormat(data)
+        console.log(test)
+    })
+}
+
+
 
 //include in whatever page this will be implemented in, so probably finalpage.handlebars
 //<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
